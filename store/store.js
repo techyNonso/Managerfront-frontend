0b1ce95eac55acc6faa03a7033da47e7527cd185
techyNonso <williamikeji@gmail.com>
2021-12-23 12:23:19 -0800

    redux middle tool removed

--- store/store.js
+++ store/store.js
@@ -11,8 +11,8 @@ export const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleWare)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
